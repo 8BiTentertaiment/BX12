9a3c09166d27623fa683179adfcfa8941128ac71
8BiTentertaiment <kysevnakysya@gmail.com>
2025-08-01 22:13:19 +0300

    Create dx12_interface.rs

---
+++ bx12/src/dx12_interface.rs
@@ -0,0 +1,13 @@
+// Interfaces and bindings for DX12 structures
+
+pub struct Dx12Device {
+    // Device internals
+}
+
+impl Dx12Device {
+    pub fn new() -> Self {
+        Dx12Device {
+            // Init
+        }
+    }
+}

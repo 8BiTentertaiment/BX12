0566a01357aedb321c40bd536127dcd36034d13d
8BiTentertaiment <kysevnakysya@gmail.com>
2025-08-01 22:03:21 +0300

    Create descriptor_heap.rs

---
+++ descriptor_heap.rs
@@ -0,0 +1,13 @@
+// DescriptorHeap manages resource descriptors (textures, buffers).
+
+pub struct DescriptorHeap {
+    // Internal state
+}
+
+impl DescriptorHeap {
+    pub fn new() -> Self {
+        DescriptorHeap {
+            // Initialization
+        }
+    }
+}

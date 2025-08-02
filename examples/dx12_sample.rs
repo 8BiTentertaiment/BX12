c79ebf8bcf6bc59aca0eada4f20f3a08d136249b
8BiTentertaiment <kysevnakysya@gmail.com>
2025-08-02 15:10:28 +0300

    Delete examples directory

--- examples/dx12_sample.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let wrapper = bx12::BX12Wrapper::new();
-
-    // Example: Initialize DX12 translation and render a frame
-    wrapper.translate_dx12_calls();
-
-    println!("DX12 translation sample completed.");
-}
